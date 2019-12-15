a273030e2ec343ca95c2aba19d7a5af7dba039fb
Bharat-34 <bharatkumargude@gmail.com>
2019-12-15 20:25:31 +0530

    Print added  in app delegate

--- Quin Demo/AppDelegate.swift
+++ Quin Demo/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         
-        print("bharat Quin Task")
+        print("bharattttttt Quin Task")
         return true
     }
 
